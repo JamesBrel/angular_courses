83f8f72b86fa937a3afdf7cac64ba7f7726418fa
The Coder <thecoder38@gmail.com>
2024-06-19 09:22:31 +0100

    Building the first page displaying the Pokémon list

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 *Changelog created using the [Simple Changelog](https://marketplace.visualstudio.com/items?itemName=tobiaswaelde.vscode-simple-changelog) extension for VS Code.*
 
+## [1.1.0 ==> Building the first page displaying the Pokémon list] - 2024-06-19
+### Added
+- Added materialize css to define the Pokémon list.
+
+### Changed
+- app.component.spec is commented out so that you can work without unit tests for a start.
+
+
 ## [1.0.0 ==> Initial project] - 2024-06-15
 ### Added
 - Angular-cli installation
--- project/pokemonsApp/src/app/app.component.ts
+++ project/pokemonsApp/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './interfaces/pokemon.js';
+import { POKEMONS } from './mock-pokemons.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'pokemonsApp';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+  }
+
+  onSelectPokemon(pokemonId: string) {
+    let pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+
+    if (pokemon) {
+      console.log(`vous avez demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`vous avez demandé un pokemon qui n'existe pas.`);
+    }
+  }
 }
